a52e5ef2b9a903d22bc49fd9e7c1112f88a2b8e0
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2020-08-22 22:44:42 -0300

    Allow click

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import tileImage from './media/tiles.png'
 
 const initialTiles = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const Tile = ({ value, active }) => {
+const Tile = ({ value, active, onClick }) => {
   const commonStyles = {
     height: '80px',
     width: '80px',
@@ -12,24 +12,29 @@ const Tile = ({ value, active }) => {
     borderRadius: '4px',
     fontSize: '60px',
     color: 'red',
+    outline: 'none',
+    cursor: 'pointer',
   }  
   const activeStyles = {
     backgroundColor: '#f19838',
     border: '1px',
   }
   const inactiveStyles = {
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    borderStyle: 'none',
   }
 
   return (
-    <div
-    className="flex justify-center items-center"
-    style={{
-      ...commonStyles,
-      ...(active ? activeStyles : inactiveStyles),
-    }}>
+    <button
+      className="flex justify-center items-center"
+      style={{
+        ...commonStyles,
+        ...(active ? activeStyles : inactiveStyles),
+      }}
+      onClick={onClick}
+    >
     {active && value}
-    </div>
+    </button>
   )
 }    
 
@@ -64,7 +69,13 @@ function App() {
       <img src={tileImage} alt="tiles" />
       <p style={{ fontSize: '36px' }}>Possible tiles:</p>
       <div className="flex">
-        {initialTiles.map((tile) => <Tile value={tile} active={tiles.includes(tile)} />)}
+        {initialTiles.map((tile) => (
+          <Tile
+            value={tile}
+            active={tiles.includes(tile)}
+            onClick={() => setTiles((t) => t.includes(tile) ? t.filter((x) => x !== tile) : t.concat(tile).sort())}
+          />
+        ))}
       </div>
       <button className="mt5"onClick={handleReset}>Reset</button>
     </div>
